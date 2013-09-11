093a0112eed7a51a6446c31350104fcf8cc847f7
insumy <rodion@dev.softaddicts.com>
2013-09-11 10:16:31 +0300

    Initialize project

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.11'
+gem 'mysql2'
+gem 'webrick', '~> 1.3.1'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem 'bootstrap-sass'
+gem 'cancan'
+gem 'devise'
+gem 'figaro'
+gem 'rolify'
+gem 'simple_form'
+gem 'slim'
+group :development do
+  gem 'better_errors'
+  gem 'quiet_assets'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reports::Application.config.secret_token = '07cea82e3c3db721397745b67bd7bd639a162892acbc7394fe51f41f58d6cef4f2e33fd738bdbf7ec4bb19b02d262ac99597e333dc2c053c24d15f9db15a0a29'
