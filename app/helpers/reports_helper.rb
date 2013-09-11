65529725a7c84543c2b5c5cd015e9114f37bc6b6
insumy <rodion@dev.softaddicts.com>
2013-09-11 16:16:57 +0300

    sorting reports list

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,6 @@
 module ReportsHelper
+  def sort_to(column)
+    direction = (params[:sort] == column && params[:direction] == 'desc') ? 'asc' : 'desc'
+    link_to(column.capitalize, { sort: column, direction: direction }, class: params[:sort] == column ? direction : nil)
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,12 @@ class Report < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
   paginates_per 5
+
+  class << self
+    def sort_by(column, direction)
+      order_column = Report.column_names.include?(column) ? column : 'created_at'
+      direction = %w(asc desc).include?(direction) ? direction : 'desc'
+      order(order_column + ' ' + direction)
+    end
+  end
 end
